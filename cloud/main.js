9c26ed0112ba2c1a9459b49bf6ea2e19b4ff0528
travis rother <travis@duffekmobile.com>
2017-05-05 02:44:07 -0400

    character results

--- cloud/main.js
+++ cloud/main.js
@@ -214,7 +214,7 @@ Parse.Cloud.afterSave('LogSession', function(request, response) {
             }
 
             for(charName in characterResults) {
-                var character = characterResults[mapName];
+                var character = characterResults[charName];
                 for(mapName in character) {
 
                     var record = character[mapName];
